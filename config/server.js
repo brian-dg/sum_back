32830e52e26ae3795c3b3eac273603108b5f7e85
brian-dg <brian_rc6@hotmail.com>
2022-03-09 12:45:53 -0300

    configuracion

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'af4b72cbd755ae37e48d16ba53ca3524'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cda3f96f-28eb-4379-9a62-e46fce6029ef'
+};
